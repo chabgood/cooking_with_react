7382439e024522254517d70bc536e47554e85b3c
Chris Habgood <chabgood@gmail.com>
2019-12-30 23:48:25 -0600

    css left side

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,29 +3,31 @@ import IngredientList from "./IngredientList";
 
 export default function Recipe({ ...props }) {
   return (
-    <div>
-      <div>
-        <h3>{props.name}</h3>
+    <div className="recipe">
+      <div className="recipe__header">
+        <h3 className="recipe__title">{props.name}</h3>
         <div>
-          <button>Edit</button>
-          <button>Delete</button>
+          <button className="btn btn--primary mr-1">Edit</button>
+          <button className="btn btn--danger">Delete</button>
         </div>
       </div>
-      <div>
-        <span>Servings</span>
-        <span>{props.servings}</span>
+      <div className="recipe__row">
+        <span className="recipe__label">Cooking Time</span>
+        <span className="recipe__value">{props.cookTime}</span>
       </div>
-      <div>
-        <span>Cooking Time</span>
-        <span>{props.cookTime}</span>
+      <div className="recipe_row">
+        <span className="recipe__label">Servings</span>
+        <span className="recipe__value">{props.servings}</span>
       </div>
-      <div>
-        <span>Instructions:</span>
-        <div>{props.instructions}</div>
+      <div className="recipe_row">
+        <span className="recipe__label">Instructions:</span>
+        <div className="recipe__value recipe__instructions recipe_value--indented">
+          {props.instructions}
+        </div>
       </div>
-      <div>
-        <span>Ingredients:</span>
-        <div>
+      <div className="recipe_row">
+        <span className="recipe__label">Ingredients:</span>
+        <div className="recipe__value recipe__instructions recipe_value--indented">
           <IngredientList ingredients={props.ingredients} />
         </div>
       </div>
